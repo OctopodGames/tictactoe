a7adbe8034786cc14cd499f4a262cf18e584a31d
Zach Tirrell <zbtirrell@gmail.com>
2013-03-04 22:40:48 -0500

    reworked the JS to better model an object structure

--- dom/ttt.js
+++ dom/ttt.js
@@ -1,96 +1,108 @@
 $(function() {
-	drawBoard();
+	ttt.init();
+});
 
-	$(document).on('click', '.cell', function(e){
+
+var ttt = {
+	turn: 'X',
+	turns: 0,
+	
+	init: function() { 
+		this.resetBoard();
+	
+		$(document).on('click', '.cell', ttt.takeTurn );
+	},
+	
+	takeTurn: function(e) {	
 		e.preventDefault();
 
-		if ( $(this).hasClass('taken') )
-		{
+		if ( $(this).hasClass('taken') ) {
 			return;
 		}// end if
 
-		$(this).text( turn );
-		$(this).addClass( 'taken ' + turn );
+		$(this).text( ttt.turn );
+		$(this).addClass( 'taken ' + ttt.turn );
 
-		if ( checkWin( turn ) )
-		{
-			window.alert( turn + ' wins!!' );
+		if ( ttt.checkWin( ttt.turn ) ) {
+			$('<h3 id="win">' + ttt.turn + ' wins!</h3>').insertBefore('#board');
 		}// end if
-
-		changeTurn();
-	});
-});
-
-var turn = 'X';
-var turns = 0;
-
-function checkWin()
-{
-	turns++;
-	if ( turns < 5	)
-	{
-		return false;
-	}// end if
-
-	for( i=1; i<=3; i++ )
-	{
-		// win in a row
-		selector = '.row' + i + ' .taken.' + turn;
-		if( $( selector ).length == 3 )
-		{
-			return true;
-		}// end if
-
-		// win in a column
-		selector = '.col' + i + '.taken.' + turn;
-		if( $( selector ).length == 3 )
-		{
-			return true;
-		}// end if
-	}// end for
-
-	// diagonal, top left to bottom right
-	selector = '.row1 .col1.taken.' + turn +', .row2 .col2.taken.' + turn +', .row3 .col3.taken.' + turn;
-	if( $( selector ).length == 3 )
-	{
-		return true;
-	}// end if
-
-	// diagonal, top right to bottom left
-	selector = '.row1 .col3.taken.' + turn +', .row2 .col2.taken.' + turn +', .row3 .col1.taken.' + turn;
-	if( $( selector ).length == 3 )
-	{
-		return true;
-	}// end if
-
-	return false;
-}
-
-function changeTurn()
-{
-	if ( 'X' == turn )
-	{
-		turn = 'O';
-	}// end if
-	else
-	{
-		turn = 'X';
-	}// end else
-}
-
-function drawBoard()
-{
-	var grid_size = 3;
-	var board='';
-	for(i = 1; i <= grid_size; i++)
-	{
-		board += '<div class="row' + i + '">';
-		for(j = 1; j <= grid_size; j++)
-		{
-			board += '<div class="cell col' + j + '">&nbsp;</div>';
+		else {
+			ttt.changeTurn();
 		}
-		board += '</div>';
-	}
+	},
+
+	checkWin: function () {
+		this.turns++;
+		if ( this.turns < 5	)
+		{
+			return false;
+		}// end if
 	
-	$('#board').html(board);
+		for( i=1; i<=3; i++ )
+		{
+			// win in a row
+			selector = '.row' + i + ' .taken.' + this.turn;
+			if( $( selector ).length == 3 )
+			{
+				return true;
+			}// end if
+	
+			// win in a column
+			selector = '.col' + i + '.taken.' + this.turn;
+			if( $( selector ).length == 3 )
+			{
+				return true;
+			}// end if
+		}// end for
+	
+		// diagonal, top left to bottom right
+		selector = '.row1 .col1.taken.' + this.turn +', .row2 .col2.taken.' + this.turn +', .row3 .col3.taken.' + this.turn;
+		if( $( selector ).length == 3 )
+		{
+			return true;
+		}// end if
+	
+		// diagonal, top right to bottom left
+		selector = '.row1 .col3.taken.' + this.turn +', .row2 .col2.taken.' + this.turn +', .row3 .col1.taken.' + this.turn;
+		if( $( selector ).length == 3 )
+		{
+			return true;
+		}// end if
+	
+		return false;
+	},
+
+	changeTurn: function ()
+	{
+		if ( 'X' == this.turn )
+		{
+			this.turn = 'O';
+		}// end if
+		else
+		{
+			this.turn = 'X';
+		}// end else
+	},
+
+	resetBoard: function ()
+	{
+		this.turn = 'X';
+		this.turns = 0;
+
+		$('#win').remove();
+		
+		var grid_size = 3;
+		var board='';
+		for(i = 1; i <= grid_size; i++)
+		{
+			board += '<div class="row' + i + '">';
+			for(j = 1; j <= grid_size; j++)
+			{
+				board += '<div class="cell col' + j + '">&nbsp;</div>';
+			}
+			board += '</div>';
+		}
+		
+		$('#board').html(board);
+	}
 }
