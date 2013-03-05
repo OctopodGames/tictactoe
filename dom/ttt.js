13afabb7cfd9feeb9d4d210bb4667da0b30f37b0
Zach Tirrell <zbtirrell@gmail.com>
2013-03-04 22:42:46 -0500

    consistent braces

--- dom/ttt.js
+++ dom/ttt.js
@@ -33,59 +33,49 @@ var ttt = {
 
 	checkWin: function () {
 		this.turns++;
-		if ( this.turns < 5	)
-		{
+		if ( this.turns < 5	) {
 			return false;
 		}// end if
 	
-		for( i=1; i<=3; i++ )
-		{
+		for( i=1; i<=3; i++ ) {
 			// win in a row
 			selector = '.row' + i + ' .taken.' + this.turn;
-			if( $( selector ).length == 3 )
-			{
+			if( $( selector ).length == 3 ) {
 				return true;
 			}// end if
 	
 			// win in a column
 			selector = '.col' + i + '.taken.' + this.turn;
-			if( $( selector ).length == 3 )
-			{
+			if( $( selector ).length == 3 ) {
 				return true;
 			}// end if
 		}// end for
 	
 		// diagonal, top left to bottom right
 		selector = '.row1 .col1.taken.' + this.turn +', .row2 .col2.taken.' + this.turn +', .row3 .col3.taken.' + this.turn;
-		if( $( selector ).length == 3 )
-		{
+		if ( $( selector ).length == 3 ) {
 			return true;
 		}// end if
 	
 		// diagonal, top right to bottom left
 		selector = '.row1 .col3.taken.' + this.turn +', .row2 .col2.taken.' + this.turn +', .row3 .col1.taken.' + this.turn;
-		if( $( selector ).length == 3 )
-		{
+		if( $( selector ).length == 3 ) {
 			return true;
 		}// end if
 	
 		return false;
 	},
 
-	changeTurn: function ()
-	{
-		if ( 'X' == this.turn )
-		{
+	changeTurn: function () {
+		if ( 'X' == this.turn ) {
 			this.turn = 'O';
 		}// end if
-		else
-		{
+		else {
 			this.turn = 'X';
 		}// end else
 	},
 
-	resetBoard: function ()
-	{
+	resetBoard: function () {
 		this.turn = 'X';
 		this.turns = 0;
 
@@ -93,16 +83,14 @@ var ttt = {
 		
 		var grid_size = 3;
 		var board='';
-		for(i = 1; i <= grid_size; i++)
-		{
+		for ( i = 1; i <= grid_size; i++ ) {
 			board += '<div class="row' + i + '">';
-			for(j = 1; j <= grid_size; j++)
-			{
+			for(j = 1; j <= grid_size; j++) {
 				board += '<div class="cell col' + j + '">&nbsp;</div>';
 			}
 			board += '</div>';
 		}
 		
-		$('#board').html(board);
+		$('#board').html( board );
 	}
 }
